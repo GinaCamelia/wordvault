5869b335306d0ee1bcc6eb0d1648867f956e7f6d
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-15 13:17:55 +0000

    added temporary jumbotron inside a newly created file for testing purposes

---
+++ src/components/Hello.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Hello() {
+  return (
+    <div className="main-container">
+      <div className="container">
+        <div className="jumbotron jumbotron-fluid">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <hr class="my-4" />
+          <p>
+            It uses utility classes for typography and spacing to space content
+            out within the larger container.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
