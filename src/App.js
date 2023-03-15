8decbeca2215d4509d3de1c5dd78359d97252c02
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-15 14:17:20 +0000

    imported 2 components into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Hello from './components/Hello';
+import Header from './components/Header/index.js';
+import SearchEngine from './components/SearchEngine/index.js';
 
 function App() {
   return (
     <div>
-      <Hello />
+      <Header />
+      <SearchEngine />
     </div>
   );
 }
