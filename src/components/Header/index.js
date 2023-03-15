5633f7d7c65db95f1df29737eefd76daff0a1846
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-15 14:18:24 +0000

    created new components and their css file for header and search bar

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function Header(){
+    return(
+        <div className='header'>
+            <h1>Hello</h1>
+        </div>
+    )
+}
