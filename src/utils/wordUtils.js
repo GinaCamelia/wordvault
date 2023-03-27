d97b918d2212095e875fa7dc3045f02b20b0ecfd
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-27 10:32:29 +0200

    to keep my code DRY (don't repeat yourself), and avoid duplicating the same logic across multiple components, created a separate component that accepts the data array as a parameter and a callback function that defines the logic for filtering and transforming the data. This way, I can pass different callback functions for different components to customise the behavior of the function

---
+++ src/utils/wordUtils.js
@@ -0,0 +1,14 @@
+export const filteredWords = (data, filteredData) => {
+  return data
+    .filter((word) => word.word.length > 2)
+    .sort(() => Math.random() - 0.5)
+    .reduce((filteredData, word) => {
+      if (filteredData.length >= 10) {
+        return filteredData;
+      }
+      if (!filteredData.includes(word.word)) {
+        filteredData.push(word.word);
+      }
+      return filteredData;
+    }, []);
+};
