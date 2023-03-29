f06dc40b43dfaffc8346b51ab7c3907e0df0d663
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-29 13:36:18 +0200

    Implement a React component called Photos that takes in a photos prop and conditionally renders a grid of photos with clickable links to their original source if the photos prop is not null or undefined. The photos are displayed in a grid of 3 columns and an appropriate aspect ratio is maintained for landscape photos

---
+++ src/components/Photos.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <div className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} className="img-fluid" alt="" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
