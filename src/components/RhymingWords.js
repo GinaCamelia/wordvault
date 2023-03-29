b083125542d0c6bb47d01a9ec7dccf99a7a9bef6
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-29 13:41:32 +0200

    added funtionality to component to stop the app from making api calls as soon as user starts inputing a value

--- src/components/RhymingWords.js
+++ src/components/RhymingWords.js
@@ -5,23 +5,32 @@ import { fetchDataFromAPI } from "../utils/ApiUtils";
 
 export default function RhymingWords(props) {
   const [rhymes, setRhymes] = useState([]);
+  const [showResults, setShowResults] = useState(false);
 
   // declared an effect to be run when the props.value changes
   useEffect(() => {
+    if(!props.value) return;
     console.log(props);
     // declared an asyncronous function to to fetch synonyms from api
     async function fetchRhymingWords() {
+      const endpoints = ["rel_rhy"];
+      const params = [props.value];
       // imported function from apiUtils.js
-      const apiUrl = await fetchDataFromAPI('rel_rhy', props.value);
+      const apiUrl = await fetchDataFromAPI(endpoints, params);
       // console.log('The API url is ' + apiUrl);
       const data = await apiUrl;
       console.log(data);
       // imported component from wordUtils.js
       const rhymes = filteredWords(data);
       setRhymes(rhymes);
+      setShowResults(true);
     }
-    // call the function when the value changes
-    fetchRhymingWords();
+    if (props.value) {
+      fetchRhymingWords();
+    } else {
+      setRhymes([]);
+      setShowResults(false);
+    }
     // by adding props to the dependency array, React is re-running the effect whenever
     // props or props.value changes
   }, [props.value, props]);
@@ -30,11 +39,13 @@ export default function RhymingWords(props) {
   return (
     <div className="SearchContainer">
       <div className="response">
-        <ul>
-          {rhymes.map((rhyme) => (
-            <li key={rhyme}>{rhyme}</li>
-          ))}
-        </ul>
+        {showResults && (
+          <ul>
+            {rhymes.map((rhyme) => (
+              <li key={rhyme}>{rhyme}</li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
