abbc7f57e103a60d4badffdeb9cb9f36c307e0e7
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-29 13:59:13 +0200

    Refactored fetchDataFromAPI function to handle multiple endpoints and parameters, and to check for JSON content type in the response.

--- src/utils/ApiUtils.js
+++ src/utils/ApiUtils.js
@@ -1,16 +1,9 @@
-export async function fetchDataFromAPI(endpoint, param) {
+export async function fetchDataFromAPI(endpoints, params) {
   try {
-    const response = await fetch(
-      `https://api.datamuse.com/words?${endpoint}=${param}`
-    );
-    console.log(response);
-    const contentType = response.headers.get("content-type");
-    if (contentType && contentType.indexOf("application/json") !== -1) {
-      const data = await response.text();
-      return JSON.parse(data);
-    } else {
-        console.log('The response is not in JSON format');
-    }
+    const queries = endpoints.map((endpoint, index) => `${endpoint}=${params[index]}`).join('&');
+    const response = await fetch(`https://api.datamuse.com/words?${queries}`);
+    const data = await response.text();
+    return JSON.parse(data);
   } catch (error) {
     console.log(error);
   }
