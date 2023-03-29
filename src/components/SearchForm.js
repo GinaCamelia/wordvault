0a55d5eabb534c7d9707c19f978f6c369fca42ee
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-29 15:32:38 +0200

    moved Pexels API inside the useEffect hook to display images for the default keyword and any other input, changed api parameter, and set the default state of the api to true to pull the data and then to false to stop rendering before search is clicked

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,11 +4,12 @@ import Synonyms from "./Synonyms";
 import RhymingWords from "./RhymingWords";
 import MeansLike from "./MeansLike";
 import Photos from "./Photos";
+import Meronyms from "./Meronyms";
 import axios from "axios";
 
 export default function SearchForm(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
-  const [showResults, setShowResults] = useState(false);
+  const [loaded, setLoaded] = useState(true);
   const [photos, setPhotos] = useState([]);
   const [defaultData, setDefaultData] = useState({});
 
@@ -22,92 +23,113 @@ export default function SearchForm(props) {
 
   useEffect(() => {
     async function fetchDefaultData() {
-      const endpoints = ["rel_rhy", "rel_syn", "ml"];
-      const params = [keyword, keyword, keyword];
+      const apiKey = "563492ad6f91700001000001bb77275a5ea44eceb6658854a019ff59";
+      const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+      let headers = { Authorization: apiKey };
+      axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+      const endpoints = ["rel_rhy", "rel_syn", "ml", "rel_par"];
+      const params = [keyword, keyword, keyword, keyword];
       const apiUrl = `https://api.datamuse.com/words?${endpoints
         .map((e, i) => `${e}=${params[i]}`)
         .join("&")}`;
       const response = await axios.get(apiUrl);
       const defaultData = {
-        rhymingWords: response.data.filter((item) =>
-          item.hasOwnProperty("word")
-        ),
+        rhymingWords: response.data.filter((item) =>item.hasOwnProperty("word")),
         synonyms: response.data.filter((item) => item.hasOwnProperty("word")),
         meansLike: response.data.filter((item) => item.hasOwnProperty("word")),
+        meronyms: response.data.filter((item) => item.hasOwnProperty("word")),
       };
       setDefaultData(defaultData);
-      setShowResults(true);
+      // setLoaded(false);
     }
     fetchDefaultData();
   }, [keyword]);
 
   const search = (event) => {
     event.preventDefault();
-    setShowResults(true);
-    const apiKey = "563492ad6f91700001000001bb77275a5ea44eceb6658854a019ff59";
-    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let headers = { Authorization: apiKey };
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+    setLoaded(false);
   };
 
-  return (
-    <div className="SearchContainer">
-      <form onSubmit={search} className="SearchForm">
-        <input
-          type="search"
-          placeholder="Searching for ..."
-          className="form-control SearchInput"
-          onChange={handleChange}
-          value={keyword}
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-primary SearchButton"
-        />
-      </form>
-      <p className="hint">
-        <span>Sugestions:</span> sunset, sunrise, forest, tennis...
-      </p>
+  const whenLoaded = () => {
+    setLoaded(false);
+    search();
+  };
 
-      <div className="RhymingWords response">
-        <h4>Words that rhyme: </h4>
-        {showResults && (
-          <RhymingWords
+  if (loaded) {
+    return (
+      <div className="SearchContainer">
+        <form onSubmit={search} className="SearchForm">
+          <input
+            type="search"
+            placeholder="Searching for ..."
+            className="form-control SearchInput"
+            onChange={handleChange}
             value={keyword}
-            setKeyword={setKeyword}
-            key={"rhy"}
-            data={defaultData.rhymingWords}
           />
-        )}
-      </div>
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary SearchButton"
+          />
+        </form>
+        <p className="hint">
+          <span>Sugestions:</span> sunset, sunrise, forest, tennis...
+        </p>
 
-      <div className="Synonyms response">
-        <h4>Synonyms:</h4>
-        {showResults && (
-          <Synonyms
-            value={keyword}
-            setKeyword={setKeyword}
-            key={"syn"}
-            data={defaultData.synonyms}
-          />
-        )}
-      </div>
+        <div className="RhymingWords response">
+          <h4>Words that rhyme: </h4>
+          {/* {loaded && ( */}
+            <RhymingWords
+              value={keyword}
+              setKeyword={setKeyword}
+              key={"rhy"}
+              data={defaultData.rhymingWords}
+            />
+          {/* )} */}
+        </div>
 
-      <div className="MeansLike response">
-        <h4> Means Like:</h4>
-        {showResults && (
-          <MeansLike
-            value={keyword}
-            setKeyword={setKeyword}
-            key={"ml"}
-            data={defaultData.meansLike}
-          />
-        )}
+        <div className="Synonyms response">
+          <h4>Synonyms:</h4>
+          {/* {loaded && ( */}
+            <Synonyms
+              value={keyword}
+              setKeyword={setKeyword}
+              key={"syn"}
+              data={defaultData.synonyms}
+            />
+          {/* )} */}
+        </div>
+
+        <div className="MeansLike response">
+          <h4> Means Like:</h4>
+          {/* {loaded && ( */}
+            <MeansLike
+              value={keyword}
+              setKeyword={setKeyword}
+              key={"ml"}
+              data={defaultData.meansLike}
+            />
+          {/* )} */}
+        </div>
+
+        <div className="Homophones response">
+          <h4>Direct meronyms(part of):</h4>
+          {/* {loaded && ( */}
+            <Meronyms
+              value={keyword}
+              setKeyword={setKeyword}
+              key={"par"}
+              data={defaultData.meronyms}
+            />
+          {/* )} */}
+        </div>
+
+        <div className="Photos">
+          <Photos photos={photos} />
+        </div>
       </div>
-      <div className="Photos">
-        <Photos photos={photos} />
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return whenLoaded();
+  }
 }
