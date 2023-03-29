ff0d9aedf1c6090e4b145e9279b22fe2264cce89
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-29 13:38:37 +0200

    wrapped the app in a container for better handleing and changed the default keyword to 'sunshine'

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import SearchForm from './components/SearchForm.js';
 
 function App() {
   return (
-    <div className='container'>
+    <div className='appContainer'>
       <div className='Header'>
         <h1>WORDVAULT</h1>
       </div>
-      <SearchForm  />
+      <SearchForm defaultKeyword='sunshine'/>
     </div>
   );
 }
