9a03c968e92fd3e6f1811f2ce048a05a27d6dd7d
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-29 14:01:05 +0200

    refactored code after modifying the api function and added fucntionality to stop the api from making calls for each letter of the value being input

--- src/components/Synonyms.js
+++ src/components/Synonyms.js
@@ -3,38 +3,38 @@ import "./style.css";
 import { filteredWords } from "../utils/WordUtils";
 import { fetchDataFromAPI } from "../utils/ApiUtils";
 
-// added synonym feature to component using Datamuse API
 export default function Synonyms(props) {
   const [synonyms, setSynonyms] = useState([]);
+  const [showResults, setShowResults] = useState(false);
 
-  // declared an effect to be run when the props.value changes
   useEffect(() => {
-    console.log(props);
-    // declared an asyncronous function to to fetch synonyms from api
     async function fetchSynonyms() {
-      const apiUrl = await fetchDataFromAPI('rel_syn', props.value);
-      console.log(apiUrl);
+      const endpoints = ['rel_syn'];
+      const params = [props.value];
+      const apiUrl = await fetchDataFromAPI(endpoints, params);
       const data = await apiUrl;
-      console.log(data);
-      //filter, sort and reduce the data to a list of up to 10 unique words
       const synonyms = filteredWords(data);
       setSynonyms(synonyms);
+      setShowResults(true);
     }
-    // call the function when the value changes
-    fetchSynonyms();
-    // by adding props to the dependency array, React is re-running the effect whenever
-    // props or props.value changes
+    if (props.value) {
+      fetchSynonyms();
+    } else {
+      setSynonyms([]);
+      setShowResults(false);
+    }
   }, [props.value, props]);
 
-  //render div that contains a list of synonyms
   return (
     <div className="SearchContainer">
       <div className="response">
-        <ul>
-          {synonyms.map((synonym) => (
-            <li key={synonym}>{synonym}</li>
-          ))}
-        </ul>
+        {showResults && (
+          <ul>
+            {synonyms.map((synonym) => (
+              <li key={synonym}>{synonym}</li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
