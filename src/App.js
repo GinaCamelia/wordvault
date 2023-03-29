c4135832d1cd2867528f137fb4f5894fae71d3c5
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-29 15:29:18 +0200

    changed default keyword

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className='Header'>
         <h1>WORDVAULT</h1>
       </div>
-      <SearchForm defaultKeyword='sunshine'/>
+      <SearchForm defaultKeyword='trunk'/>
     </div>
   );
 }
