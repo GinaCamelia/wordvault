e65e62c9eb9253f64fb54aba0190c5bf9e0fd060
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-30 19:51:50 +0100

    created new file with double a in it to ensure github is tracking the changes

---
+++ src/utils/aapiUtils.js
@@ -0,0 +1,10 @@
+export async function fetchDataFromAPI(endpoints, params) {
+    try {
+      const queries = endpoints.map((endpoint, index) => `${endpoint}=${params[index]}`).join('&');
+      const response = await fetch(`https://api.datamuse.com/words?${queries}`);
+      const data = await response.text();
+      return JSON.parse(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
