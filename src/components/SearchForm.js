5d59b3470b676995ac7aa5f06051b28ce86b7272
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-03-31 12:09:56 +0100

    adjusted search bar and suggestions section layout to be more responsive

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -22,7 +22,8 @@ export default function SearchForm(props) {
 
   return (
     <div className="container SearchContainer">
-      <div className=" col-lg-12 d-flex justify-content-center ">
+      <div className="d-flex flex-column align-items-center">
+      <div className="col-lg-12 d-flex justify-content-center ">
         <form onSubmit={search} className="SearchForm">
           <input
             type="search"
@@ -38,11 +39,12 @@ export default function SearchForm(props) {
           className="btn btn-primary SearchButton"
         />
       </div>
-      <div className="d-flex, flex-wrap justify-content-center">
+      <div className="d-flex flex-wrap justify-content-center">
         <p className="hint mt-1 mb-5 d-flex flex-row justify-content-center">
           <span>Suggestions:&nbsp;</span> sunset, sunrise, sunshine, sunflower, trunk, tree
           ...
         </p>
+      </div>
       </div>
 
       <div className="row">
